150d5813cbc4f7c0b5a8b2b1d29d9625f0377a1f
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-07-13 11:15:57 +0530

    Added test for post loginHandler

--- test/testLoginHandler.js
+++ test/testLoginHandler.js
@@ -8,13 +8,24 @@ const config = {
     path: 'public',
   },
 };
+const req = request(app(config));
 
-describe('GET /login', () => {
-  it('Should should give back an html', (done) => {
-    request(app(config))
-      .get('/login')
-      .expect('Content-type', /html/)
-      .expect(200, done)
+describe('/login', () => {
+  describe('GET', () => {
+    it('Should should give back an html', (done) => {
+      req
+        .get('/login')
+        .expect('Content-type', /html/)
+        .expect(200, done);
+    });
+  });
+  describe('POST', () => {
+    it('Should give 304 when no username specified', (done) => {
+      req
+        .post('/login')
+        .send('age=10')
+        .expect(304, done)
+    });
   });
 });
 
